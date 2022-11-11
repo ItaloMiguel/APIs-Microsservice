d94c259429d526f447991db5e04cb7c2e0a37739
ItaloMiguel <italomiguelleite16@gmail.com>
2022-11-11 18:18:35 -0300

    "Fazendo o CRUD"

---
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/model/Pagamento.java
@@ -0,0 +1,51 @@
+package br.com.alurafood.pagamentos.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "pagamentos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Pagamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Positive
+    private BigDecimal valor;
+
+    @NotBlank
+    @Size(max = 100)
+    private String nome;
+
+    @NotBlank
+    @Size(max = 19)
+    private String numero;
+
+    @NotBlank
+    @Size(max = 7)
+    private String expiracao;
+
+    @NotBlank
+    @Size(min = 3, max = 3)
+    private String codigo;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @NotNull
+    private Long pedidoId;
+
+    @NotNull
+    private Long formaDePagamentoId;
+}
