65bd2eaa70f5e164653ee763467a1a5969ada0dc
ItaloMiguel <italomiguelleite16@gmail.com>
2022-11-14 20:17:49 -0300

    Restartando o projeto por que do pom

--- course/src/main/java/br/com/microsservice/course/endpoint/service/CourseService.java
+++
@@ -1,8 +0,0 @@
-package br.com.microsservice.course.endpoint.service;
-
-import br.com.microsservice.course.endpoint.model.Course;
-import org.springframework.data.domain.Pageable;
-
-public interface CourseService {
-    public Iterable<Course> list(Pageable pageable);
-}
