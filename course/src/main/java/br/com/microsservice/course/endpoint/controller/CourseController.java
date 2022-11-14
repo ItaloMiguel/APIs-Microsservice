798edba779f646999b5a8d1daed5935a440800b7
ItaloMiguel <italomiguelleite16@gmail.com>
2022-11-14 19:27:39 -0300

    "Fazendo um ListALl para testar"

--- course/src/main/java/br/com/microsservice/course/endpoint/controller/CourseController.java
+++ course/src/main/java/br/com/microsservice/course/endpoint/controller/CourseController.java
@@ -1,4 +1,31 @@
 package br.com.microsservice.course.endpoint.controller;
 
+import br.com.microsservice.course.endpoint.model.Course;
+import br.com.microsservice.course.endpoint.service.CourseService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping("/v1/admin/course")
 public class CourseController {
+
+    private final CourseService courseService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Iterable<Course>> list(@PageableDefault(value = 10) Pageable pageable) {
+        return ResponseEntity.ok(courseService.list(pageable));
+    }
+
 }
--- course/src/main/java/br/com/microsservice/course/endpoint/service/CourseService.java
+++ course/src/main/java/br/com/microsservice/course/endpoint/service/CourseService.java
@@ -1,4 +1,8 @@
 package br.com.microsservice.course.endpoint.service;
 
+import br.com.microsservice.course.endpoint.model.Course;
+import org.springframework.data.domain.Pageable;
+
 public interface CourseService {
+    public Iterable<Course> list(Pageable pageable);
 }
